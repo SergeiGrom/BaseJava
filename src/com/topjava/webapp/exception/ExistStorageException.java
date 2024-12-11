21d059f7f5f3c00e8352a03f823cc2cdcff78f6e
Sergei_Grom <morfeysg@gmail.com>
2024-12-11 17:44:24 +1000

    exception fix

--- src/com/topjava/webapp/exception/ExistStorageException.java
+++ src/com/topjava/webapp/exception/ExistStorageException.java
@@ -2,6 +2,6 @@ package com.topjava.webapp.exception;
 
 public class ExistStorageException extends StorageException{
     public ExistStorageException(String uuid) {
-        super("Resume UUID = [" + uuid + "] NOT EXIST", uuid);
+        super("Resume UUID = [" + uuid + "] ALREADY EXIST", uuid);
     }
 }
--- src/com/topjava/webapp/exception/NotExistStorageException.java
+++ src/com/topjava/webapp/exception/NotExistStorageException.java
@@ -1,6 +1,6 @@
 package com.topjava.webapp.exception;
 
-public class NotExistStorageException extends StorageException{
+public class NotExistStorageException extends StorageException {
     public NotExistStorageException(String uuid) {
         super("Resume UUID = [" + uuid + "] NOT EXIST", uuid);
     }
