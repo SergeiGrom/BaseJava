9f39f0c04f7378397902b21fc45f46c55902f90d
Sergei_Grom <morfeysg@gmail.com>
2024-12-25 15:55:33 +1000

    Lesson 7: add DateUtil.class

---
+++ src/com/topjava/webapp/util/DateUtil.java
@@ -0,0 +1,9 @@
+package com.topjava.webapp.util;
+
+import java.time.LocalDate;
+
+public class DateUtil {
+    public static LocalDate of(int year, int month) {
+        return LocalDate.of(year, month, 1);
+    }
+}
