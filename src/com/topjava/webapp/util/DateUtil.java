801dd72c2610e15f4d48651bee2fe52e00502725
Sergei_Grom <morfeysg@gmail.com>
2025-01-15 18:33:57 +1000

    Lesson 8: add LocalDate NOW

--- src/com/topjava/webapp/util/DateUtil.java
+++ src/com/topjava/webapp/util/DateUtil.java
@@ -1,9 +1,12 @@
 package com.topjava.webapp.util;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 public class DateUtil {
-    public static LocalDate of(int year, int month) {
+    public static final LocalDate NOW = LocalDate.of(3000, Month.of(1), 1);
+
+    public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
     }
 }
